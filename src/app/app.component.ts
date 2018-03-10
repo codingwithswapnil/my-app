342a31c115d71f74d494c412fb7bec61ab0ca2c2
swapnilwaman <swap.waman@gmail.com>
2018-03-10 10:38:46 +0530

    Home page changes
    Home page changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  title = 'Swapnil\'s Web page';
+  dateText:string;
+  buttonFlag:boolean;
+  hobbiesFlag:boolean;
+  hobbies: string[];
+  hobbyButtonText:string;
+  defaultHobbies: string[];
+
+  constructor(){
+    this.dateText = 'Hover on blue button to see the date.';
+    this.buttonFlag = false;
+    this.hobbies = ['Reading','Music','Coding'];
+    this.defaultHobbies = ['Reading','Music','Coding'];
+    this.hobbiesFlag = true;
+    this.hobbyButtonText = 'hide hobbies';
+  }
+
+  buttonClick(){
+    this.buttonFlag = true;
+  }
+
+  closeClick(){
+    this.buttonFlag = false;
+  }
+
+  mouseOver(){
+    this.dateText = new Date().toString();
+  }
+
+  mouseOut(){
+    this.dateText = 'Hover on blue button to see the date';
+  }
+
+  addHobby(){
+    this.hobbies.push('New Hobby');
+    // this.hobbies.unshift('New Item');
+  }
+  
+  resetHobby(){
+    this.hobbies = ['Reading','Music','Coding'];
+  }
+  
+  deleteHobby(i){
+    this.hobbies.splice(i,1);
+  }
+
+  hobbyClick(){
+    if(!this.hobbiesFlag){
+      this.hobbiesFlag = true;
+      this.hobbyButtonText = 'hide hobbies';
+    }
+    else{
+      this.hobbiesFlag = false;
+      this.hobbyButtonText = 'show hobbies';  
+    }
+  }
 }
