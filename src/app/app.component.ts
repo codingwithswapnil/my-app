c6f85061b1293e5fd3297fdd561477a4ea269dc9
swapnilwaman <swap.waman@gmail.com>
2018-03-11 10:31:38 +0530

    New User class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from './User';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +16,32 @@ export class AppComponent {
   hobbies: string[];
   hobbyButtonText:string;
   defaultHobbies: string[];
+  activities: string[];
+  books: any[];
+  booksFlag: boolean;
+  booksButtonText: string;
+  addBooksFlag: boolean;
+  addBooksButtonText: string;
+  user: User;
 
   constructor(){
+    this.user = new User();
     this.dateText = 'Hover on blue button to see the date.';
     this.buttonFlag = false;
     this.hobbies = ['Reading','Music','Coding'];
     this.defaultHobbies = ['Reading','Music','Coding'];
     this.hobbiesFlag = true;
     this.hobbyButtonText = 'hide hobbies';
+    this.booksFlag = true;
+    this.booksButtonText = 'Hide Books';
+    this.books = [
+      {id: 1, name: 'Test 1', auth: 'Auth 1', price: 10, class: 'success'},
+      {id: 2, name: 'Test 2', auth: 'Auth 2', price: 20, class: 'warning'},
+      {id: 3, name: 'Test 3', auth: 'Auth 3', price: 30, class: 'info'},
+      {id: 4, name: 'Test 4', auth: 'Auth 4', price: 40, class: 'active'}
+    ];
+    this.addBooksFlag = false;
+    this.addBooksButtonText = "Show add book form";
   }
 
   buttonClick(){
@@ -62,5 +82,38 @@ export class AppComponent {
       this.hobbiesFlag = false;
       this.hobbyButtonText = 'show hobbies';  
     }
+  }
+
+  viewbooks(){
+    
+    if(!this.booksFlag)
+      this.booksButtonText = 'Hide Books';
+    else
+      this.booksButtonText = 'Show Books';
+    this.booksFlag = !this.booksFlag;
+  }
+
+  deleteBook(i){
+    this.books.splice(i,1);
+  }
+
+  resetBooks(){
+    
+  }
+
+  addBook(){
+    if(!this.addBooksFlag)
+      this.addBooksButtonText = "Hide add book form";
+    else
+      this.addBooksButtonText = 'Show add book form';
+    this.addBooksFlag = !this.addBooksFlag;
+  }
+
+  saveBookInfo(){
+    this.books.push(
+      this.user
+    );
+
+    // this.frmBooks.reset();
   }
 }
